c0c86c9976dd74100bb14ef65bcd38da76d1436b
rafakimanja <90486707+rafakimanja@users.noreply.github.com>
2024-02-06 14:56:22 -0300

    new commit

---
+++ clientes/validators.py
@@ -0,0 +1,19 @@
+from validate_docbr import CPF
+import re
+
+def cpf_valido(numero_cpf):
+    cpf = CPF()
+    return cpf.validate(numero_cpf)
+
+def nome_valido(nome):
+    return nome.isalpha()
+
+def rg_valido(rg):
+    return len(rg) == 9
+
+def celular_valido(celular):
+    """Verifica se o celular é valido usando expressão regular
+        Num: (11 91234-1234)"""
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta = re.findall(modelo, celular)
+    return resposta
